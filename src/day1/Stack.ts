3fe5a8a1974089f478e8b3ab71090694e27216d7
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-28 23:50:16 +0000

    Stack data structure

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,39 @@
+type Node<T> = {
+    val: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        const newNode = { val: item } as Node<T>;
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        newNode.prev = this.head;
+        this.head = newNode;
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) return undefined;
+        this.length--;
+        const poppedItem = this.head;
+        if (this.length === 0) {
+            this.head = undefined;
+            return poppedItem.val;
+        }
+        this.head = this.head.prev;
+        poppedItem.prev = undefined;
+        return poppedItem.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
