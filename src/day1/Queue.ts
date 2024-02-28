1fd9432d5b097c2036a6ae1d808508b357297c8e
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-28 23:40:51 +0000

    queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
-
-}
+        this.length++;
+        const newNode = { val: item } as Node<T>;
+        if (!this.tail) {
+            this.tail = this.head = newNode;
+            return;
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
     deque(): T | undefined {
-
-}
+        if (!this.head || this.length === 0) {
+            return undefined;
+        }
+        this.length--;
+        const dequeuedItem = this.head;
+        this.head = this.head.next;
+        dequeuedItem.next = undefined;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+        return dequeuedItem.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
