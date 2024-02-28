0d7768a1be159a0dac1f247c7ae84ff6b436b6cb
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-28 18:39:17 +0000

    binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    while (low < high) {
+        const midPoint = Math.floor(low + (high - low) / 2);
+        const value = haystack[midPoint];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = midPoint;
+        } else {
+            low = midPoint + 1;
+        }
+    }
+    return false;
 }
