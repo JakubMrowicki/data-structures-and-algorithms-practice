ebeee4aa980e97936355d6c5d385ed3b39068b9a
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-28 19:48:08 +0000

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,18 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
 }
+
+// O(N^2)
+/*
+How it works:
+[1,4,3,2]
+[1,3,4,2]
+[1,3,2,4]
+[1,2,3,4]
+*/
