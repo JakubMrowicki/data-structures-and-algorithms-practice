0de7f4b4004e7bc2e95ada2a1292939d97e5f6b8
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-29 12:51:34 +0000

    quick sort implementation

--- src/__tests__/QuickSort.ts
+++ src/__tests__/QuickSort.ts
@@ -2,8 +2,11 @@ import quick_sort from "@code/QuickSort";
 
 test("quick-sort", function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
+    const arr2 = [10, 9, 8, 7, 6, 1, 4, 3, 2, 5];
 
     debugger;
     quick_sort(arr);
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+    quick_sort(arr2);
+    expect(arr2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,31 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        //console.log(`${lo} >= ${hi}`);
+        return;
+    }
+    const pivotIdx = partition(arr, lo, hi);
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+}
 
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+
+    idx++;
+    [arr[hi], arr[idx]] = [arr[idx], arr[hi]];
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    console.log(`Sorting ${arr} \nLow: ${0}\nHigh: ${arr.length - 1}`);
+    qs(arr, 0, arr.length - 1);
 }
